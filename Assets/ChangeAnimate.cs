a24f78f0eee1ed00bf76238a607a7d00075d2400
Sarawut355 <srwt1902@hotmail.com>
2022-02-26 01:11:16 +0700

    first commit

---
+++ Assets/ChangeAnimate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimate : MonoBehaviour
+{
+  public void getAnimatorOrcWalk()
+    {
+        this.GetComponent<Animator>().Play("Orc Walk");
+    }
+  public void getAnimatorRightTurn()
+    {
+        this.GetComponent<Animator>().Play("Right Turn");
+    }
+  public void getAnimatorDrunkWalkBackwards()
+    {
+        this.GetComponent<Animator>().Play("Drunk Walk Backwards");
+    }
+    public void getAnimatorCockyHeadTurn()
+    {
+        this.GetComponent<Animator>().Play("Cocky Head Turn");
+    }
+    public void getAnimatorDencing()
+    {
+        this.GetComponent<Animator>().Play("Dancing Twerk");
+    }
+    public void getAnimatorRumba()
+    {
+        this.GetComponent<Animator>().Play("Rumba Dancing");
+    }
+}
---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ChangeColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void getBrown()
+    {
+        GetComponent<Renderer>().material.color = new Color(58f / 255f, 12f / 255f, 12f / 255f);
+    }
+    public void getGreen()
+    {
+        GetComponent<Renderer>().material.color = new Color(0f / 255f, 56f / 255f, 0f / 255f);
+    }
+    public void getYello()
+    {
+        GetComponent<Renderer>().material.color = Color.yellow;
+    }
+    public void getBlack()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+    public void getEnter1()
+    {
+        GetComponent<Renderer>().material.color = new Color(255f / 255f, 0f / 255f, 226f / 255f);
+    }
+    public void getEnter2()
+    {
+        GetComponent<Renderer>().material.color = new Color(0f / 255f, 195f / 255f, 255f / 255f);
+    }
+}
